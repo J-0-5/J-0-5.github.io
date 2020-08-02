34c51c1296b094f2d78064a06a6c859d7d4a0a63
J-0-5 <66178697+J-0-5@users.noreply.github.com>
2020-08-01 20:39:06 -0500

    Update contactenos.js

--- js/contactenos.js
+++ js/contactenos.js
@@ -1,61 +1,52 @@
-window.onload = function (){
-    div = document.getElementById('show');
-    div.style.display = 'none';
+const clientes = []
 
-    const form = document.getElementById("form"),
-    nombre1 = document.getElementById("nombre1"),
+const nombre1 = document.getElementById("nombre1"),
     apellido1 = document.getElementById("apellido1"),
-    direccion = document.getElementById("direccion"), 
+    dir = document.getElementById("direccion"),
     cedula = document.getElementById("nodoc"),
-    telefono = document.getElementById("telefono"),
-    mostrar = document.getElementById("mostrar"); 
-    const clientes = []
+    tel = document.getElementById("telefono");
 
+div = document.getElementById('show');
+div.style.display = 'none';
 
+const form = document.getElementById("form"),
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault(); 
-        const nuevocliente = {
-            nombre1 : nombre1.value,
-            apellido1: apellido1.value, 
-            cedula: cedula.value,
-            direccion : direccion.value, 
-            telefono: telefono.value,
-            
-    } 
+    mostrar = document.getElementById("mostrar");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nuevocliente = {
+        cliente: nombre1.value + " " + apellido1.value,
+        identificacion: cedula.value,
+        direccion: dir.value,
+        telefono: tel.value
+    }
+
     div = document.getElementById('show');
     div.style.display = '';
 
-    clientes.splice(0,0,nuevocliente)
-    localStorage.setItem('listclientes', JSON.stringify(clientes))
+    clientes.splice(0, 0, nuevocliente)
+    localStorage.setItem('newC', JSON.stringify(clientes));
 
     tbody = document.querySelector('#mostrar tbody')
-    mostrar.addEventListener('click', () => {
-    let fila = tbody.insertRow(0),
-    nombre1 = fila.insertCell(0),
-    apellido1 = fila.insertCell(1),
-    cedula = fila.insertCell(2), 
-    direccion = fila.insertCell(3),
-    telefono = fila.insertCell(4)
-    eliminar = fila.insertCell(5)
+        let fila = tbody.insertRow(0),
+            cliente = fila.insertCell(0),
+            identificacion = fila.insertCell(1),
+            direccion = fila.insertCell(2),
+            telefono = fila.insertCell(3),
+            eliminar = fila.insertCell(4);
 
+            cliente.innerHTML = nuevocliente.cliente
+            identificacion.innerHTML = nuevocliente.identificacion
+            direccion.innerHTML = nuevocliente.direccion
+            telefono.innerHTML = nuevocliente.telefono
+            eliminar.innerHTML = '<button id="btn" name="btn" onclick="delRow(this)" > Delete</button>';
 
-    nombre1.innerHTML = nuevocliente.nombre1;
-    apellido1.innerHTML = nuevocliente.apellido1;
-    cedula.innerHTML = nuevocliente.cedula;
-    direccion.innerHTML = nuevocliente.direccion;
-    telefono.innerHTML = nuevocliente.telefono;
-    eliminar.innerHTML ='<button id="btn" name="btn" onclick="delRow(this)" > Delete</button>';
-    }); 
+});
 
- 
-    function delRow(currElement) {
-        var parentRowIndex = currElement.parentNode.parentNode.rowIndex;
-        document.getElementById("mostrar").deleteRow(parentRowIndex);
-        clientes.splice(parentRowIndex-1,1);
-        localStorage.setItem('table', JSON.stringify(clientes));
-    } 
-    
-})
-} 
-
+function delRow(currElement) {
+    var parentRowIndex = currElement.parentNode.parentNode.rowIndex;
+    document.getElementById("mostrar").deleteRow(parentRowIndex);
+    clientes.splice(parentRowIndex - 1, 1);
+    localStorage.setItem('newC', JSON.stringify(clientes));
+}
